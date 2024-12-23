32f419afba16ec934bbd1c08fda2c14c38c27d08
Aymen Kadri <82622888+aymendev8@users.noreply.github.com>
2024-12-23 14:44:18 +0100

    Update contact page with LinkedIn link

--- app/contact/page.js
+++ app/contact/page.js
@@ -8,7 +8,10 @@ export default function Home() {
         <div className="pt-20 flex flex-col items-center text-center">
           <h4 className="text-white text-6xl mb-8">Hi ! I'M STILL MAT</h4>
           <div className="text-2xl space-y-4">
-            <a href="" className="text-white underline block">
+            <a
+              href="https://www.linkedin.com/in/math%C3%A9o-peyrot-des-gachons-3146b6327/"
+              className="text-white underline block"
+            >
               Linkedin
             </a>
             <a
