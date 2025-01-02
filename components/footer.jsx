b88ab0b51ad22e1a5e4de2e4853c32594143a98b
Aymen Kadri <82622888+aymendev8@users.noreply.github.com>
2025-01-02 16:07:44 +0100

    Refactor email address in contact page

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="flex flex-col justify-center items-center py-16 px-4 font-aldrich">
       <div className="text-white/60 text-2xl space-y-2">
         <p>Paris France</p>
-        <p>m.peyrotdesactions@tu-strate.design</p>
+        <p>m.peyrotdesgachons@strate.design</p>
         <p>+33782679917</p>
       </div>
     </footer>
